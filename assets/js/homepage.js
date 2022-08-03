ca73129f8fd3fb7ea562d8139d9be168936d7ba3
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-08-03 14:26:49 -0700

    adding API for repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,15 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + "jonathonb1" + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
 
+// 6.2.1
